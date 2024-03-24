f093c8e0cc30bcef7a07a17009b29e9b983b2197
Carlos Mertens <mertenscarlos3000@gmail.com>
2024-03-24 20:24:21 +0100

    Create body validator middleware

--- dist/routes/toursRouter.js
+++ dist/routes/toursRouter.js
@@ -6,10 +6,18 @@ const tours_1 = require("../controllers/tours");
 const validateParam_1 = require("../middlewares/validateParam");
 exports.toursRouter = (0, express_1.Router)();
 exports.toursRouter.param('id', validateParam_1.validateParam);
+// TODO: Refactor with joi validation
+const validateBodyReq = (req, res, next) => {
+    if (!req.body.name)
+        return res
+            .status(400)
+            .send({ status: 'fail', data: {}, message: 'Name is required' });
+    next();
+};
 exports.toursRouter
     .route('/')
     .get(tours_1.controllers.getAllTours)
-    .post(tours_1.controllers.createNewTour);
+    .post(validateBodyReq, tours_1.controllers.createNewTour);
 exports.toursRouter
     .route('/:id')
     .get(tours_1.controllers.getTour)
--- src/routes/toursRouter.ts
+++ src/routes/toursRouter.ts
@@ -1,4 +1,4 @@
-import {Router} from 'express';
+import {RequestHandler, Router} from 'express';
 import {controllers} from '../controllers/tours';
 import {validateParam} from '../middlewares/validateParam';
 
@@ -6,10 +6,20 @@ export const toursRouter = Router();
 
 toursRouter.param('id', validateParam);
 
+// TODO: Refactor with joi validation
+const validateBodyReq: RequestHandler = (req, res, next) => {
+  if (!req.body.name)
+    return res
+      .status(400)
+      .send({status: 'fail', data: {}, message: 'Name is required'});
+
+  next();
+};
+
 toursRouter
   .route('/')
   .get(controllers.getAllTours)
-  .post(controllers.createNewTour);
+  .post(validateBodyReq, controllers.createNewTour);
 
 toursRouter
   .route('/:id')
